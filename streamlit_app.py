c5faa540ee0990e222db45613454e9e23f1b9294
Kenny Jade <ntdkhoa@hotmail.com>
2024-10-19 09:44:06 +0700

    20241019-100000

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,20 @@
 import streamlit as st
 import pandas as pd
+import streamlit as st
+
+# Initialize connection.
+conn = st.connection("postgresql", type="sql")
+
+# Perform query.
+df = conn.query('select * from history where stock = "ACB" order by date desc', ttl="10m")
 
 # Title of the app
 st.title("Stock Data Dashboard")
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row}")
+    print(row)
+
 
 # Load stock data from database or API
 data = {
@@ -12,7 +24,16 @@ data = {
 df = pd.DataFrame(data)
 
 # Display data as a table
-st.table(df)
+#st.table(df)
 
 # Line chart of stock prices
-st.line_chart(df.set_index('Date')['Price'])
+#st.line_chart(df.set_index('Date')['Price'])
+# Create a sidebar with a title and text input
+#st.sidebar.title("Stock Input Panel")
+#stock_name = st.sidebar.text_input("Enter stock name:")
+
+
+# Display the input from the sidebar on the main page
+#st.write(f"You have entered: {stock_name}")
+
+# You can add more elements to the sidebar as needed

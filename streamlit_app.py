1eb726abde69638aa74ede954311f8d60c9089ec
Kenny Jade <ntdkhoa@hotmail.com>
2024-10-23 21:25:49 +0700

    20241023_213000

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ def get_stock_data(stock_name):
     df['bp'] = df['buy'] / (df['buy'] + df['sell'] + df['neutral'])
     df['np'] = df['neutral'] / (df['buy'] + df['sell'] + df['neutral'])
     df['sp'] = df['sell'] / (df['buy'] + df['sell'] + df['neutral'])
-    #df['date'] = df['date'].astype(str)
+    df['Date'] = df['date'].astype(str)
 
     # Ensure the 'Date' column is in datetime format (adjust if your schema has a different name for the date column)
     #if 'Date' in df.columns:
@@ -70,8 +70,8 @@ if 'selected_stock' not in st.session_state:
 
 # Left Panel Layout
 with st.sidebar:
-    st.sidebar.title("Stock Input Panel")
-    stock_name_input = st.sidebar.text_input("Nhập mã chứng khoán:")
+    #st.sidebar.title("Stock Input Panel")
+    stock_name_input = st.sidebar.text_input("INPUT YOUR STOCK:")
     #stock_name = st.text_input("## nhập mã cổ phiếu")
 
     if stock_name_input:
@@ -79,47 +79,26 @@ with st.sidebar:
         #st.query_params.selected_stock=stock_name_input)  # Keep the selection state
 
 # Main Content: Display the query result on the right
-st.title("Stock Data Dashboard")
+# st.title("Stock Data Dashboard")
 if st.session_state.selected_stock:
-    st.write(f"Kết quả phân tích: {st.session_state.selected_stock}")
+    st.write(f"{st.session_state.selected_stock} daily analysis:  ")
     stock_data = get_stock_data(st.session_state.selected_stock)
 
     if not stock_data.empty:        
-        hover = alt.selection_single(
-            fields=["date"],
-            nearest=True,
-            on="mouseover",
-            empty="none",
-        )
 
         # Display line chart
         recommend_order = ['STRONG_BUY', 'BUY', 'NEUTRAL', 'SELL', 'STRONG_SELL']
-        lines = alt.Chart(stock_data).mark_line(point=True).encode(
-            x=alt.X('date:T', axis=alt.Axis(title='', format='%d/%m/%Y')),
-            y=alt.Y('recommendation:N', title=None, sort=recommend_order),
-            color='stock',
-            #color=alt.Color('stock', sort=recommend_order),
-            #order=alt.Order('y_site_sort_index:Q')
+
+        # Create the point chart
+        chart = alt.Chart(stock_data).mark_point(size=100).encode(
+            x=alt.X('Date:O', axis=None),  # Ordinal x-axis to only display provided dates
+            y=alt.Y('recommendation:N', axis=alt.Axis(title=''), sort=recommend_order),  # Nominal y-axis for recommendations
+            tooltip=[alt.Tooltip('stock:N'), alt.Tooltip('recommendation:N'), alt.Tooltip('Date:O')]  # Tooltips for interactivity
         ).properties(
-            width=600,  # Customize the width
-            height=400  # Customize the height
+            width=600,
+            height=200
         )
-
-        points = lines.transform_filter(hover).mark_circle(size=65)
-        tooltips = (
-            alt.Chart(stock_data)
-            .mark_rule()
-            .encode(
-                x="yearmonthdate(date)",
-                y=alt.Y('recommendation', sort=recommend_order),
-                opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
-                tooltip=[alt.Tooltip("date:T", title="Ngày"), alt.Tooltip("recommendation", title="Khuyến nghị"), alt.Tooltip("stock", title="Mã CP"),],
-            ).add_params(hover)
-        )
-
-        data_layer = lines + points + tooltips
-        st.altair_chart(data_layer, use_container_width=True)
-
+        st.altair_chart(chart, use_container_width=True)
 
         # Display stack chart
         # Reshape the DataFrame using pd.melt() to convert the bp, np, sp columns to a single 'Rating' column
@@ -131,21 +110,21 @@ if st.session_state.selected_stock:
         rating_map = {'bp': 'Buy', 'np': 'Neutral', 'sp': 'Sell'}
         df_melted['Rating'] = df_melted['Rating'].map(rating_map)
         # Convert 'Date' column to datetime
-        df_melted['Date'] = pd.to_datetime(df_melted['date'])
-        #df_melted['Date'] = df_melted['date'].astype(str)
-
+        #df_melted['Date'] = pd.to_datetime(df_melted['date'])
+        df_melted['Date'] = df_melted['date'].astype(str)
+        
         # Define custom colors for the ratings
-        color_scale = alt.Scale(
+        area_color = alt.Scale(
             domain=['Buy', 'Neutral', 'Sell'],
             range=['#00ff00', '#ffff00', '#ff0000']
         )
 
         area_chart = alt.Chart(df_melted).mark_area().encode(
-            x=alt.X('Date:O', title="", axis=None),
+            x=alt.X('Date:O', title="", axis=alt.Axis(title='')),
             y=alt.Y('sum(Percentage):Q', title="", axis=None),
-            color=alt.Color('Rating:N', scale=color_scale),  # Custom colors for ratings
+            color=alt.Color('Rating:N', scale=area_color).legend(orient="left"),  # Custom colors for ratings
             opacity={"value": 0.7},
-            tooltip=[alt.Tooltip('stock:N', title="Mã CP"), alt.Tooltip('date:T', title="Ngày"), alt.Tooltip('Rating:N', title="Khuyến nghị"), alt.Tooltip('Percentage:Q', format='.0%', title="Xác suất")]  # Tooltips for interactivity
+            tooltip=[alt.Tooltip('stock:N', title="Stock"), alt.Tooltip('date:T', title="Date"), alt.Tooltip('Rating:N', title="Recommendation"), alt.Tooltip('Percentage:Q', format='.0%', title="Ratio")]  # Tooltips for interactivity
         ).properties(
             width=800,
             height=300
